d2c42c815ff814decb6d3a927745a023abc11814
Rahul Chatterjee <44574873+lymntrix@users.noreply.github.com>
2020-04-09 10:22:56 +0530

    Add files via upload

---
+++ basic_input_output.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int greeknum;
+cout<<"enter any your age : ";
+cin>>greeknum;
+greeknum=greeknum +40;
+cout<<"your fathers age is approx.. :"<<greeknum<<endl;
+
+}
+
