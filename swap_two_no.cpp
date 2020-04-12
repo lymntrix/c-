ac065353c672a07dbff589a5926b91fb01407357
Rahul Chatterjee <44574873+lymntrix@users.noreply.github.com>
2020-04-12 10:13:31 +0530

    Add files via upload

---
+++ swap_two_no.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a=5,b=7, temp;
+	cout<<"numbers befor swaping"<<endl;
+	cout<<"a ="<< a << " b = "<<b<<endl;
+	temp=a;
+	a=b;
+	b=temp;
+	cout<<"numbers after swaping"<<endl;
+	cout<<" a ="<<a<<" b= "<<b<<endl;
+	return 0;
+	
+}
