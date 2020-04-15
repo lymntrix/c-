8521743ae2e917228fa8891f4d29823624401e9e
Rahul Chatterjee <44574873+lymntrix@users.noreply.github.com>
2020-04-15 13:02:50 +0530

    Add files via upload

---
+++ prime_no.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n,i,count=0;
+	cout<<"enter the number";
+	cin>>n;
+	for (i=1;i<=n;i++)
+	{
+		if(n%i==0){
+			count++;
+	}	
+	}
+	if(count==2)
+	{
+	cout<<n<<" is a prime number";
+	}
+	else
+	{
+	cout<<n<<" is not a prime";
+return 0;
+}
+}
